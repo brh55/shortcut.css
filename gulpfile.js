02f51f84d484d3235b923e863d1ed224f7b18122
Brandon Him <brh55@drexel.edu>
2015-09-19 19:36:12 -0400

    Initial partial files and gulp set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,33 @@
 // Build Dependencies
+var autoprefix	= require('gulp-autoprefixer');
+var autoprefix  = require('autoprefixer');
+var concatCSS	= require('gulp-concat-css');
+var cssnano 	= require('cssnano');
 var gulp        = require('gulp');
 var postcss     = require('gulp-postcss');
-var scsslint 	= require('gulp-scss-lint');
-var cssnano 	= require('cssnano');
 var precss		= require('precss');
-
+var rename		= require('gulp-rename');
+var scsslint 	= require('gulp-scss-lint');
 
 gulp.task('lint', function () {
   return gulp.src('/src/*.css')
-    .pipe(scsslint());
+    .pipe(scsslint({
+    	'config': '.scss-lint.yml'
+    }))
+    .pipe(scsslint.failReporter());
 });
 
 gulp.task('build', ['lint'], function () {
     var processors = 
    	[
+    	precss(),
     	cssnano(),
-    	precss()
+    	autoprefix()
     ];
     return gulp.src('./src/*.css')
 	    .pipe(postcss(processors))
+	    .pipe(concatCSS('bundle.css'))
+	    .pipe(rename('hackCSS.min.css'))
 	    .pipe(gulp.dest('./dest/css'));
 });
 
