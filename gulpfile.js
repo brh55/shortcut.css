0d5d79b7bc2d31bcf86398d7a160ce66c9fe6119
Brandon Him <brh55@drexel.edu>
2015-09-19 18:18:58 -0400

    Initial working gulp file and proper build task
    Currently with a test css file that is building a css file with scss syntax using postCSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
 // Build Dependencies
 var gulp        = require('gulp');
 var postcss     = require('gulp-postcss');
+var scsslint 	= require('gulp-scss-lint');
+var cssnano 	= require('cssnano');
+var precss		= require('precss');
+
+
+gulp.task('lint', function () {
+  return gulp.src('/src/*.css')
+    .pipe(scsslint());
+});
+
+gulp.task('build', ['lint'], function () {
+    var processors = 
+   	[
+    	cssnano(),
+    	precss()
+    ];
+    return gulp.src('./src/*.css')
+	    .pipe(postcss(processors))
+	    .pipe(gulp.dest('./dest/css'));
+});
+
+gulp.task('default', ['build']);
