6e8a8eed94b147094f7077b7929336b87236f735
Brandon Him <brh55@drexel.edu>
2015-09-20 10:19:00 -0400

    First text-coloring rules established
    This also includes a smooth out of all the gulp build file related issues.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,34 @@
 // Build Dependencies
-var autoprefix	= require('gulp-autoprefixer');
 var autoprefix  = require('autoprefixer');
 var concatCSS	= require('gulp-concat-css');
 var cssnano 	= require('cssnano');
 var gulp        = require('gulp');
 var postcss     = require('gulp-postcss');
+var pcssEach    = require('postcss-each');
 var precss		= require('precss');
 var rename		= require('gulp-rename');
-var scsslint 	= require('gulp-scss-lint');
+var reporter    = require('postcss-reporter');
+var stylelint   = require('stylelint');
 
-gulp.task('lint', function () {
-  return gulp.src('/src/*.css')
-    .pipe(scsslint({
-    	'config': '.scss-lint.yml'
-    }))
-    .pipe(scsslint.failReporter());
-});
-
-gulp.task('build', ['lint'], function () {
-    var processors = 
-   	[
-    	precss(),
-    	cssnano(),
-    	autoprefix()
+gulp.task('build', function () {
+    var processors = [
+        precss(),
+        pcssEach(),
+    	autoprefix(),
+        cssnano(),
+        stylelint({
+            "rules": {
+                "string-quotes": [2, "double"],
+                "color-hex-case": [2, "lower"],
+                "selector-no-id": 2
+            }
+        }),
+        reporter({
+            clearMessages: true
+        })
     ];
     return gulp.src('./src/*.css')
-	    .pipe(postcss(processors))
-	    .pipe(concatCSS('bundle.css'))
+        .pipe(postcss(processors))
 	    .pipe(rename('hackCSS.min.css'))
 	    .pipe(gulp.dest('./dest/css'));
 });
